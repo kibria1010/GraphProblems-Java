61cc196854250c6d32f732f72e25c9b55fd4a591
Md. Golam Kibria <78980142+kibria1010@users.noreply.github.com>
2024-11-04 12:01:44 +0600

    Add files via upload

---
+++ EdgeWeightedDigraph.java
@@ -0,0 +1,146 @@
+package depthfirstpaths;
+
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+
+public class EdgeWeightedDigraph {
+    private static final String NEWLINE = System.getProperty("line.separator");
+
+    private final int V;                // number of vertices in this digraph
+    private int E;                      // number of edges in this digraph
+    private LinkedList<DirectedEdge>[] adj;    // adj[v] = adjacency list for vertex v
+    private int[] indegree;             // indegree[v] = indegree of vertex v
+
+    public EdgeWeightedDigraph(int V) {
+        if (V < 0) throw new IllegalArgumentException("Number of vertices in a Digraph must be non-negative");
+        this.V = V;
+        this.E = 0;
+        this.indegree = new int[V];
+        adj = (LinkedList<DirectedEdge>[]) new LinkedList[V];
+        for (int v = 0; v < V; v++)
+            adj[v] = new LinkedList<>();
+    }
+
+    /**
+     * Returns the number of vertices in this edge-weighted digraph.
+     *
+     * @return the number of vertices in this edge-weighted digraph
+     */
+    public int V() {
+        return V;
+    }
+
+    /**
+     * Returns the number of edges in this edge-weighted digraph.
+     *
+     * @return the number of edges in this edge-weighted digraph
+     */
+    public int E() {
+        return E;
+    }
+
+    // throw an IllegalArgumentException unless {@code 0 <= v < V}
+    private void validateVertex(int v) {
+        if (v < 0 || v >= V)
+            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
+    }
+
+    /**
+     * Adds the directed edge {@code e} to this edge-weighted digraph.
+     *
+     * @param  e the edge
+     * @throws IllegalArgumentException unless endpoints of edge are between {@code 0}
+     *         and {@code V-1}
+     */
+    public void addEdge(DirectedEdge e) {
+        int v = e.from();
+        int w = e.to();
+        validateVertex(v);
+        validateVertex(w);
+        adj[v].add(e);
+        indegree[w]++;
+        E++;
+    }
+
+
+    /**
+     * Returns the directed edges incident from vertex {@code v}.
+     *
+     * @param  v the vertex
+     * @return the directed edges incident from vertex {@code v} as an Iterable
+     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+     */
+    public Iterable<DirectedEdge> adj(int v) {
+        validateVertex(v);
+        return adj[v];
+    }
+
+    public int outdegree(int v) {
+        validateVertex(v);
+        return adj[v].size();
+    }
+
+    public int indegree(int v) {
+        validateVertex(v);
+        return indegree[v];
+    }
+
+    /**
+     * Returns all directed edges in this edge-weighted digraph.
+     * To iterate over the edges in this edge-weighted digraph, use foreach notation:
+     * {@code for (DirectedEdge e : G.edges())}.
+     *
+     * @return all edges in this edge-weighted digraph, as an iterable
+     */
+    public Iterable<DirectedEdge> edges() {
+        LinkedList<DirectedEdge> list = new LinkedList<>();
+        for (int v = 0; v < V; v++) {
+            for (DirectedEdge e : adj(v)) {
+                list.add(e);
+            }
+        }
+        return list;
+    }
+
+    /**
+     * Returns a string representation of this edge-weighted digraph.
+     *
+     * @return the number of vertices <em>V</em>, followed by the number of edges <em>E</em>,
+     *         followed by the <em>V</em> adjacency lists of edges
+     */
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append(V + " " + E + NEWLINE);
+        for (int v = 0; v < V; v++) {
+            s.append(v + ": ");
+            for (DirectedEdge e : adj[v]) {
+                s.append(e + "  ");
+            }
+            s.append(NEWLINE);
+        }
+        return s.toString();
+    }
+
+    /**
+     * Unit tests the {@code EdgeWeightedDigraph} data type.
+     *
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args) {
+        EdgeWeightedDigraph g = new EdgeWeightedDigraph(5);
+        DirectedEdge e1 = new DirectedEdge(0, 1, 5.67);
+        DirectedEdge e2 = new DirectedEdge(0, 2,  5.67);
+        DirectedEdge e3 = new DirectedEdge(1, 3,  5.67);
+        DirectedEdge e4 = new DirectedEdge(0, 5,  5.67);
+        DirectedEdge e5 = new DirectedEdge(0, 2,  5.67);
+        
+        g.addEdge(e1);
+        g.addEdge(e2);
+        g.addEdge(e3);
+        g.addEdge(e5);
+        g.addEdge(e5);
+        
+        System.out.println(g);
+    }
+
+}
